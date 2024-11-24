690c2aebd0603a89e2fccb878b310fdf6ab5dd17
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-11-24 21:16:36 +0900

    configure poetry settings and tasks

--- src/handlers/app.py
+++ src/handlers/app.py
@@ -2,7 +2,6 @@
 from aws_lambda_powertools import Logger, Metrics, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
-from aws_lambda_powertools.metrics import MetricUnit
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 app = APIGatewayRestResolver()
@@ -15,12 +14,10 @@ logger = Logger()
 metrics = Metrics(namespace="Powertools")
 
 
-@app.get("/hello")
+@app.get("/healthcheck", operation_id="healthcheck")
 @tracer.capture_method
-def hello():
-    metrics.add_metric(name="HelloWorldInvocations", unit=MetricUnit.Count, value=1)
-    logger.info("Hello world API - HTTP 200")
-    return {"message": "hello world"}
+def healthcheck():
+    return {"status": "ok"}
 
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
