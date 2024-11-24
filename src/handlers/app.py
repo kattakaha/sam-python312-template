2a320017c3001b5215090c41ce8bbda92b353395
Takahashi Katsuyuki <kattakaha@gmail.com>
2024-11-25 03:51:29 +0900

    feature: add github actions workflows

--- src/handlers/app.py
+++ src/handlers/app.py
@@ -3,10 +3,15 @@ from aws_lambda_powertools import Logger, Metrics, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from pydantic import BaseModel
 
 app = APIGatewayRestResolver()
 
 app.enable_swagger(path="/swagger")
+
+
+class HealthcheckResponse(BaseModel):
+    status: str
 
 
 tracer = Tracer()
@@ -16,8 +21,8 @@ metrics = Metrics(namespace="Powertools")
 
 @app.get("/healthcheck", operation_id="healthcheck")
 @tracer.capture_method
-def healthcheck():
-    return {"status": "ok"}
+def healthcheck() -> HealthcheckResponse:
+    return HealthcheckResponse(status="ok")
 
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
